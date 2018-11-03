c32e12581c4889a1bf9be24c441ef2f0bfdf6a40
lukaku3 <djangocube@gmail.com>
2018-11-03 16:02:54 +0000

    initial

--- game/views.py
+++ game/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+            'msg': 'game/index'
+        }
+        return render(request, 'game/index.html', context)
+
+index = IndexView.as_view()
+
+class changeView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+        }
+        return render(request, 'game/index.html', context)
